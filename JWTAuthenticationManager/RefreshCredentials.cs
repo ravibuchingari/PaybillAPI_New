ff11df68e5be15a2756382db8c2ce1f5a02a09e3
ravi.buchingari <ravi.buchingari@gmail.com>
2025-06-20 19:13:18 +0530

    ok

--- JWTAuthenticationManager/RefreshCredentials.cs
+++ JWTAuthenticationManager/RefreshCredentials.cs
@@ -2,8 +2,8 @@
 {
     public class RefreshCredentials
     {
-        public string JWTToken { get; set; }
-        public string RefreshToken { get; set; }
-        public string SecurityKey { get; set; }
+        public string JWTToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public string SecurityKey { get; set; } = string.Empty;
     }
 }
